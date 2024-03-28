c85b3e48a727c2a60fd618938a1a5fe87acea0af
priyanshutariyal02 <tariyalpriyanshu6@gmail.com>
2024-03-28 08:38:11 +0530

    Enhancing result modal

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,26 +1,36 @@
-import React, { forwardRef, useImperativeHandle, useRef } from 'react';
+import React, { forwardRef, useImperativeHandle, useRef } from "react";
 
-const ResultModal = forwardRef(function ResultModal({ targetTime, remainingTime, onReset }, ref) {
-
-    const dialog = useRef();
-    const userLost = remainingTime <= 0;
-    const formattedRemainingTime = (remainingTime / 1000).toFixed(2); // toFixed() : js method to format the number to a fixed number of decimal places.
-    useImperativeHandle(ref, () => {
-        return {
-            open: () => {
-                dialog.current.showModal();
-            }
-        }
-    });
-    return (
-        <dialog ref={dialog} className="result-modal">
-            {userLost && <h2>Your Lost</h2>}
-            <p>The target time was <strong>{targetTime} seconds.</strong></p>
-            <p>You stopped the timer with <strong>{formattedRemainingTime} seconds left.</strong></p>
-            <form method="dialog" onSubmit={onReset}>
-                <button>Close</button>
-            </form>
-        </dialog>
-    );
-})
+const ResultModal = forwardRef(function ResultModal(
+  { targetTime, remainingTime, onReset },
+  ref
+) {
+  const dialog = useRef();
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2); // toFixed() : js method to format the number to a fixed number of decimal places.
+  console.log(remainingTime);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
+  useImperativeHandle(ref, () => {
+    return {
+      open: () => {
+        dialog.current.showModal();
+      },
+    };
+  });
+  return (
+    <dialog ref={dialog} className="result-modal">
+      {userLost && <h2>Your Lost</h2>}
+      {!userLost && <h2>Your score: {score}</h2>}
+      <p>
+        The target time was <strong>{targetTime} seconds.</strong>
+      </p>
+      <p>
+        You stopped the timer with{" "}
+        <strong>{formattedRemainingTime} seconds left.</strong>
+      </p>
+      <form method="dialog" onSubmit={onReset}>
+        <button>Close</button>
+      </form>
+    </dialog>
+  );
+});
 export default ResultModal;
